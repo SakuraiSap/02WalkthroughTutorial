80ce8f90b13ccb3666d0708357b7c39c34ab2c01
SakuraiSap <sakurai.exam@gmail.com>
2024-03-23 15:30:14 +0900

    Step 4: XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,29 @@
+sap.ui.define([
+    'sap/ui/core/mvc/XMLView',
+], (XMLView)=>{
+    "use strict";
+    XMLView.create({
+        viewName: "ui5.walkthrough.view.App"
+    }).then(
+        (oView)=>oView.placeAt("content")
+    );
+});
+
+
+
+
+// ========== Step02 ========================
+// sap.ui.define([
+//     'sap/m/Text',
+// ], (Text) =>{
+//     "use strict";
+//     new Text({
+//         text: "Hello world Step03"
+//     }).placeAt("content")
+// });
+
+// ========== Step01 ========================-
 // sap.ui.define([], () => {
 // 	"use strict";
 // 	alert("UI5 is ready");
 // });
-sap.ui.define([
-    'sap/m/Text',
-], (Text) =>{
-    "use strict";
-    new Text({
-        text: "Hello world Step03"
-    }).placeAt("content")
-});
