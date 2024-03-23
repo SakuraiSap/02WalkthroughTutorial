e4520d7aa27755789ce6883344f4843d57f0a2d6
SakuraiSap <sakurai.exam@gmail.com>
2024-03-23 14:59:45 +0900

    Step 3: Controls

--- webapp/index.js
+++ webapp/index.js
@@ -1,4 +1,12 @@
-sap.ui.define([], () => {
-	"use strict";
-	alert("UI5 is ready");
+// sap.ui.define([], () => {
+// 	"use strict";
+// 	alert("UI5 is ready");
+// });
+sap.ui.define([
+    'sap/m/Text',
+], (Text) =>{
+    "use strict";
+    new Text({
+        text: "Hello world Step03"
+    }).placeAt("content")
 });
