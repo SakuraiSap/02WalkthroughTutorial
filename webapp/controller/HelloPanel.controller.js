e4fcca36205185f3be4d7a1ab7188932a6fa6bcf
SakuraiSap <sakurai.exam@gmail.com>
2024-03-24 14:17:41 +0900

    Step 17: Fragment Callbacks Now that we have integrated the dialog, it's time to add some user interaction. The user will definitely want to close the dialog again at some point, so we add a button to close the dialog and assign an event handler.

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -14,10 +14,16 @@ sap.ui.define([
             MessageToast.show(sMsg);
         },
         async onOpenDialog() {
+            
             this.oDialog ??= await this.loadFragment({
                 name:"ui5.walkthrough.view.HelloDialog"
             });
             this.oDialog.open();
+        },
+        onCloseDialog(){
+			// note: We don't need to chain to the pDialog promise, since this event handler
+			// is only called from within the loaded dialog itself
+            this.byId("helloDialog").close();
         }
     })
 });
