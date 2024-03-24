e96a9a0c17e500cad484dc3f7cf05dcf2b4aa840
SakuraiSap <sakurai.exam@gmail.com>
2024-03-24 11:38:09 +0900

    Step 15: Nested Views Our panel content is getting more and more complex and now it is time to move the panel content to a separate view. With that approach, the application structure is much easier to understand, and the individual parts of the app can be reused.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,35 +1,7 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast'
-    // "sap/ui/model/json/JSONModel",
-    // "sap/ui/model/resource/ResourceModel"
-], (Controller, MessageToast)=>{
+], (Controller)=>{
     'use strict';
-    return Controller.extend("ui5.walkthrough.controller.App",{
-        // onInit(){
-        //     // set data model on view
-        //     const oData = {
-        //         recipient:{
-        //             name: "World Step7"
-        //         }
-        //     };
-        //     const oModel = new JSONModel(oData);
-        //     this.getView().setModel(oModel);
-        //     // set i18n model on view
-        //     const i18nModel = new ResourceModel({
-        //         bundleName: "ui5.walkthrough.i18n.i18n"
-        //     });
-        //     this.getView().setModel(i18nModel,"i18n")
-        // },
-        onShowHello(){
-            // read msg from i18n model
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg",[sRecipient]);
-
-            MessageToast.show(sMsg);
-            //MessageToast.show("Hello world");
-            //alert("Hello world Step5");
-        }
-    })
+    return Controller.extend("ui5.walkthrough.controller.App", {
+    }); 
 });
